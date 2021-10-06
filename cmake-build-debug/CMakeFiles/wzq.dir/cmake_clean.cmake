7f7f5ccaeaa6fec5ff0012f12611e2278a7a4d21
Bobbbbbbbbbby <xiongzijun20a@mails.ucas.ac.cn>
2021-10-07 01:23:32 +0800

    all the files

---
+++ cmake-build-debug/CMakeFiles/wzq.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wzq.dir/main.c.obj"
+  "libwzq.dll.a"
+  "wzq.exe"
+  "wzq.exe.manifest"
+  "wzq.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/wzq.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
