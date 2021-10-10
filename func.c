628688109f6c2b6d66c26171a80701281c411331
Bobbbbbbbbbby <xiongzijun20a@mails.ucas.ac.cn>
2021-10-10 15:35:16 +0800

    Player vs Player finished

--- func.c
+++ func.c
@@ -2,42 +2,138 @@
 // Created by Bobby on 2021/10/9.
 //
 #include <stdio.h>
+int Vertical(int board[][19],int x,int y);
+int Horizontal(int board[][19],int x,int y);
+int An45(int board[][19],int x,int y);
+int An135(int board[][19],int x,int y);
+
+
 void Player1Draw(int board[][19])
 {
     printf("Player one Draw:");
     int x,y;
-    scanf("%d %d",x,y);
+    scanf("%d %d",&x,&y);
     for(;;)
     {
-        if (board[x][y]!=2)
+        if(!((x>=0&&x<=19)&&(y>=0&&y<=19)))
+        {
+            printf("Bad input, try again\n");
+            scanf("%d %d",&x,&y);
+        }
+        if (board[x][y]!=0)
+        {
+            printf("Bad input, try again\n");
+            scanf("%d %d",&x,&y);
+        }
+        else
         {
             board[x][y]=1;
             return;
         }
-        else if(!((x>=0&&x<=19)&&(y>=0&&y<=19)))
-        {
-            printf("Bad input, try again\n");
-            scanf("%d %d",x,y);
-        }
+
     }
-}
+}//Let player 1 put a coin
 void Player2Draw(int board[][19])
 {
+
     printf("Player two Draw:");
     int x,y;
-    scanf("%d %d",x,y);
+    scanf("%d %d",&x,&y);
     for(;;)
     {
-        if(board[x][y]!=1)
-        {
-            board[x][y] = 2;
-            return;
-        }
-        else if(!((x>=0&&x<=19)&&(y>=0&&y<=19)))
+        if(!((x>=0&&x<=19)&&(y>=0&&y<=19)))
         {
             printf("Bad input, try again\n");
-            scanf("%d %d",x,y);
+            scanf("%d %d",&x,&y);
+        }
+        if (board[x][y]!=0)
+        {
+            printf("Bad input, try again\n");
+            scanf("%d %d",&x,&y);
+        }
+        else
+        {
+            board[x][y]=2;
+            return;
         }
     }
+}//Let player 2 put a coin
+
+
+int Vertical(int board[][19],int x,int y)
+{
+    int color=board[x][y];
+    int number=1;
+    for(int xChange=x-1;board[xChange][y]==color&&xChange>=0;xChange-=1)
+        number+=1;
+    for(int xChange=x+1;board[xChange][y]==color&&xChange<=18;xChange+=1)
+        number+=1;
+    return number;
+}//Check how many coins of the same color in the same volume
+int Horizontal(int board[][19],int x,int y)
+{
+    int color=board[x][y];
+    int number=1;
+    for(int yChange=y-1;yChange>=0&&board[x][yChange]==color;yChange--)
+        number+=1;
+    for(int yChange=y+1;yChange<=18&&board[x][yChange]==color;yChange++)
+        number+=1;
+    return number;
+}//Check how many coins of the same color in the same line
+int An45(int board[][19],int x,int y)
+{
+    int color=board[x][y];
+    int number=1;
+    for(int i=1;(x-i)>=0&&(y-i)>=0&&board[x-i][y-i]==color;i++)
+        number+=1;
+    for(int i=1;(x-i)<=18&&(y-i)<=18&&board[x+i][y+i]==color;i++)
+        number+=1;
+    return number;
+}//Check how many coins of the same color in the angle 45 degrees
+int An135(int board[][19],int x,int y)
+{
+    int color=board[x][y];
+    int number=1;
+    for(int i=1;(x-i)>=0&&(y+i)<=18&&board[x-i][y+i]==color;i++)
+        number+=1;
+    for(int i=1;(x+i)<=18&&(y-i)>=0&&board[x+1][y-1]==color;i++)
+        number+=1;
+    return number;
+}//Check how many coins of the same color int the angle of 135 degrees
+
+
+int Check(int board[][19])
+{
+    for(int x=0;x<19;x++)
+    {
+        for(int y=0;y<19;y++)
+        {
+            if(board[x][y]==0)
+                continue;
+            else if(board[x][y]==1)
+            {
+                int vertical= Vertical(board,x,y);
+                int horizontal= Horizontal(board,x,y);
+                int an45= An45(board,x,y);
+                int an135= An135(board,x,y);
+                if(vertical>=5||horizontal>=5||an45>=5||an135>=5)
+                    return 1;
+                else
+                    continue;
+            }
+            else if(board[x][y]==2)
+            {
+                int vertical= Vertical(board,x,y);
+                int horizontal= Horizontal(board,x,y);
+                int an45= An45(board,x,y);
+                int an135= An135(board,x,y);
+                if(vertical>=5||horizontal>=5||an45>=5||an135>=5)
+                    return 2;
+                else
+                    continue;
+            }
+        }
+    }
+    return 0;
 }
 
--- main.c
+++ main.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
-#define Width 20
-#define Height 20
+
+#define Width 19
+#define Height 19
+
 extern void Player1Draw(int board[][19]);
+
 extern void Player2Draw(int board[][19]);
-int main()
-{
+
+extern int Check(int board[][19]);
+
+int main() {
+    setbuf(stdout, 0);
     int board[Width][Height];
-    for(int i=0;i<Width;i++)
-    {
-        for(int j=0;j<Height;j++)
-            board[i][j]=0;
+
+    int result = 0;
+    for (;;) {
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+                board[i][j] = 0;
+        }
+        for (;;) {
+            Player1Draw(board);
+            result = Check(board);
+            if (result != 0)
+                break;
+            Player2Draw(board);
+            result = Check(board);
+            if (result != 0)
+                break;
+        }
+        if (result == 1)
+            printf("Congratulations player 1, you win!\n");
+        else if (result == 2)
+            printf("Congratulations player 2, you win!\n");
+        printf("Enter r to restart, enter other characters to quit:");
+        char input;
+        scanf("%c", &input);
+        scanf("%c", &input);
+        if (input == 'r')
+            continue;
+        else {
+            printf("\n!!Terminated!!");
+            break;
+        }
     }
 }
